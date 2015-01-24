17a0c11897d0235885496acc9143c994a80cee5f
Po-Chen <pc.yang@utexas.edu>
2015-01-24 12:27:52 -0600

    Added character health prefabs and script

---
+++ Shame/Assets/Scripts/Attacker.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour {
+	GameObject player;                          // Reference to the player GameObject.
+	PlayerHealth playerHealth;                  // Reference to the player's health.
+
+	void Awake ()
+	{
+		// Setting up the references.
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerHealth = player.GetComponent <PlayerHealth> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		// If the entering collider is the player...
+		if(other.gameObject == player)
+		{
+			// If the player has health to lose...
+			if(playerHealth.currentHealth > 0)
+			{
+				// ... damage the player.
+				playerHealth.TakeDamage (10);
+			}
+		}
+	}
+}
