40f9fdfd4d45082a0640bc0bb72f5cb0bd0cfc86
Po-Chen <pc.yang@utexas.edu>
2015-01-25 06:07:07 -0600

    Added temporary Menu and Credits page

---
+++ Shame/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneTransition : MonoBehaviour {
+	public void loadScene (string scene) {
+		Application.LoadLevel(scene); // or level index
+	}
+}
