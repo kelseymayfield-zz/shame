5ea98291ccb8fa8b5b05ef16387935af30b964f2
Po-Chen <pc.yang@utexas.edu>
2015-01-25 00:29:52 -0600

    Added RoomCollider for damage and recovery over time

--- Shame/Assets/Scripts/RoomTrigger.cs
+++ Shame/Assets/Scripts/RoomTrigger.cs
@@ -1,25 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HallOfShame : MonoBehaviour {
-	public float damageOverTime;
+public class RoomTrigger : MonoBehaviour {
+	public float changeOverTime;
 	GameObject player;                          // Reference to the player GameObject.
 	PlayerHealth playerHealth;                  // Reference to the player's health.
+
 	void Awake ()
 	{
 		// Setting up the references.
 		player = GameObject.FindGameObjectWithTag ("Player");
 		playerHealth = player.GetComponent <PlayerHealth> ();
 	}
-	
+
 	void OnTriggerStay (Collider other)
 	{
 		if(other.gameObject == player)
 		{
-			if(playerHealth.currentHealth > 0)
+			float health = playerHealth.currentHealth;
+			if(health > 0.0 && health < 100.0)
 			{
-				// ... damage the player.
-				playerHealth.TakeDamage (damageOverTime);
+				float amount = -Mathf.Min(Mathf.Min (health, 100.0f - health), changeOverTime);
+				playerHealth.TakeDamage (amount);
 			}
 		}
 	}
