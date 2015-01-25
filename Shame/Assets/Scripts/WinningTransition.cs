1cb50d1ec74cae669a27e577198303cf740f72c1
Stephanie <sccheng@cs.utexas.edu>
2015-01-25 12:57:58 -0600

    Created WinGame screen

--- Shame/Assets/Scripts/WinningTransition.cs
+++ Shame/Assets/Scripts/WinningTransition.cs
@@ -19,10 +19,18 @@ public class WinningTransition : MonoBehaviour {
 			if (playerHealth.hasKey == true)
 			{
 				Debug.Log("Player entered the winning zone with key! Transiting to the wining stage...");
-				//anim.SetTrigger ("WinGame");
-				//yield return new WaitForSeconds (10f);
-				//Application.LoadLevel("Credits");
+				anim.SetTrigger ("WinGame");
+        
+        StartCoroutine(transit());
 			}
 		}
 	}
+  
+  IEnumerator transit() {
+  Debug.Log("Before Waiting 5 seconds");
+  yield return new WaitForSeconds(5);
+  Debug.Log("After Waiting 5 Seconds");
+  Application.LoadLevel("Credits");
+ }
+  
 }
